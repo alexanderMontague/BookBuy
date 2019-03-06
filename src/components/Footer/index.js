7d52950e1f357cfe4a6d90f7b2ea0df35572fa47
alexanderMontague <xturnermonty@gmail.com>
2019-03-06 01:24:37 -0500

    Finalize Main landing page styles and layout

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,9 +9,9 @@ const Footer = () => {
         <div style={{ color: "grey", margin: "auto 0" }}>&#8226;</div>
         <div className={styles.footerLink}>Postings</div>
         <div style={{ color: "grey", margin: "auto 0" }}>&#8226;</div>
-        <div className={styles.footerLink}>Sign Up!</div>
-        <div style={{ color: "grey", margin: "auto 0" }}>&#8226;</div>
         <div className={styles.footerLink}>Contact</div>
+        <div style={{ color: "grey", margin: "auto 0" }}>&#8226;</div>
+        <div className={styles.footerLink}>Sign Up!</div>
       </div>
       <div className={styles.disclaimer}>
         © Textbook Trade {new Date().getFullYear()}. All Rights Reserved.
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,11 +3,15 @@ import styles from "./styles.scss";
 
 import InputWrapper from "../../components/InputWrapper";
 import Select from "react-select";
+import { FaBook, FaRegCheckCircle, FaSearchDollar } from "react-icons/fa";
 
 import {
   schoolDropdownValues,
   programDropdownValues
 } from "../../assets/dropdownValues";
+import review1 from "../../assets/head1.jpg";
+import review2 from "../../assets/head2.jpg";
+import review3 from "../../assets/head3.jpg";
 
 class Home extends Component {
   state = {
@@ -32,6 +36,7 @@ class Home extends Component {
   render() {
     return (
       <div className={styles.homeContainer}>
+        {/* SEARCH PANEL */}
         <div className={styles.searchPanel}>
           <div className={styles.searchHeader}>
             Find the textbook you need, for cheap.
@@ -95,8 +100,71 @@ class Home extends Component {
             </form>
           </div>
         </div>
-        <div className={styles.featuresPanel}>FEATURES</div>
-        <div className={styles.otherPanel}>SOME OTHER FEATURE</div>
+
+        {/* FEATURES PANEL */}
+        <div className={styles.featuresPanel}>
+          <div className={styles.feature}>
+            <FaBook className={styles.icon} />
+            <div className={styles.featureHeader}>Find Textbooks</div>
+            <div className={styles.featureDesc}>
+              Come up with some creative shit here
+            </div>
+          </div>
+          <div className={styles.feature}>
+            <FaRegCheckCircle className={styles.icon} />
+            <div className={styles.featureHeader}>Easy to Use</div>
+            <div className={styles.featureDesc}>say how easy it is nicky</div>
+          </div>
+          <div className={styles.feature}>
+            <FaSearchDollar className={styles.icon} />
+            <div className={styles.featureHeader}>Find the Lowest Prices</div>
+            <div className={styles.featureDesc}>
+              one more time, hit me with some magic
+            </div>
+          </div>
+        </div>
+
+        {/* INFO PANEL */}
+        <div className={styles.infoPanel}>
+          <div className={styles.infoRow}>
+            <div className={styles.textBlock}>
+              <div className={styles.textTitle}>About Textbook Trade</div>
+              <div className={styles.textDesc}>Blah blah blah</div>
+            </div>
+            <div className={[styles.pictureBlock, styles.books].join(" ")} />
+          </div>
+          <div className={[styles.infoRow, styles.reverseCol].join(" ")}>
+            <div className={[styles.pictureBlock, styles.money].join(" ")} />
+            <div className={styles.textBlock}>
+              <div className={styles.textTitle}>This is what we do...</div>
+              <div className={styles.textDesc}>Blah blah blah</div>
+            </div>
+          </div>
+        </div>
+
+        {/* REVIEWS PANEL */}
+        <div className={styles.reviewsPanel}>
+          <div className={styles.reviewTitle}>What people are saying...</div>
+          <div className={styles.reviewContainer}>
+            <div className={styles.review}>
+              <img className={styles.reviewPic} src={review1} />
+              <div className={styles.reviewHeader}>Alex M</div>
+              <div className={styles.reviewDesc}>This shits hot</div>
+            </div>
+            <div className={styles.review}>
+              <img className={styles.reviewPic} src={review2} />
+              <div className={styles.reviewHeader}>Nick B</div>
+              <div className={styles.reviewDesc}>holy fuck I love this</div>
+            </div>
+            <div className={styles.review}>
+              <img className={styles.reviewPic} src={review3} />
+              <div className={styles.reviewHeader}>Bobby J</div>
+              <div className={styles.reviewDesc}>
+                i save so much on textbooks I buy more blow!
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
