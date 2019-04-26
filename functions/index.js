ea7b7770e351a652c758d04122316a7ad04fdd40
alexanderMontague <xturnermonty@gmail.com>
2019-04-25 22:12:24 -0400

    update cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,61 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 const db = admin.firestore();
 
-var mailgun = require("mailgun-js")({
-  apiKey: "950a7ad2cc727d81542c37ee91d239db-dc5f81da-5984f649",
-  domain: "bookbuy.ca"
+const config = functions.config();
+
+const mailgun = require("mailgun-js")({
+  apiKey: config.bookbuy.mailgun_api_key,
+  domain: config.bookbuy.mailgun_domain
 });
 
+// send user welcome email on new user creation
 exports.sendWelcomeEmail = functions.firestore
   .document("users/{userId}")
   .onCreate((snap, context) => {
     const user = snap.data();
-    console.log("USER", user);
 
     mailgun.messages().send(
       {
         from: "Book Buy <info@bookbuy.ca>",
         to: user.email,
         subject: "Welcome to Book Buy!",
-        userFullName: user.fullName,
-        text: "This is a test"
+        template: "welcome",
+        "v:fullName": user.fullName
       },
       (error, body) => {
         console.log("ERR", error, "BOD", body);
       }
     );
+
+    return 0;
+  });
+
+// send user an email if they recieve a new first chat message
+exports.sendNewMessageEmail = functions.firestore
+  .document("messages/{userId}")
+  .onCreate((snap, context) => {
+    const messageData = snap.data();
+    db.collection("users")
+      .where("id", "==", messageData.recipient)
+      .get()
+      .then(messageDoc => {
+        const messageData = messageDoc[0].data();
+
+        mailgun.messages().send(
+          {
+            from: "Book Buy <info@bookbuy.ca>",
+            to: reciever.email,
+            subject: "You have a new message on BookBuy!",
+            template: "new_chat",
+            "v:message": messageData.messages[0].content
+          },
+          (error, body) => {
+            console.log("ERR", error, "BOD", body);
+          }
+        );
+      });
 
     return 0;
   });
