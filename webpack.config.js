a79ccde0ee1b69c89f9d5cae88bf3eb6c724494c
alexanderMontague <xturnermonty@gmail.com>
2020-02-01 19:19:02 -0500

    add clean webpack pligin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = () => {
@@ -59,7 +60,8 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: "./src/index.html"
       }),
-      new Dotenv()
+      new Dotenv(),
+      new CleanWebpackPlugin()
     ]
   };
 };
